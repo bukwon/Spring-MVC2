d58e0e4d77d48d107ff51b290be9fce5aa1489a2
BuKwon Kim <dnu05043@naver.com>
2024-06-17 23:27:55 +0900

    URL

--- src/main/java/hello/thymeleaf/basic/basicControlller.java
+++ src/main/java/hello/thymeleaf/basic/basicControlller.java
@@ -76,6 +76,12 @@ public class basicControlller {
         return "basic/date";
     }
 
+    @GetMapping("link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
 
     @Data
     static class User  {
